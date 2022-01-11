9361009a54163e3613214d9f4686decfb76f4eb3
felipevillajr <felipevilla2010@live.com>
2022-01-10 20:06:19 -0500

    added links to work

--- src/App.js
+++ src/App.js
@@ -18,7 +18,18 @@ function App() {
         onClick={toggleClass}
         className={hide ? "home__showbox" : "home__box"}
       >
-        <h1>box</h1>
+        <h1>Examples of my work</h1>
+        <div>
+          <a href="https://scratch.mit.edu/projects/626326222">
+            Scratch Based Game
+          </a>
+          <a href="https://codesandbox.io/s/spacemanparallax-qut1r?file=/src/styles.css">
+            custom made parallax effects
+          </a>
+          <a href="https://pacific-retreat-51764.herokuapp.com/">
+            RPG style message board
+          </a>
+        </div>
       </div>
     </div>
   );
